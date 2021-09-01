edeaa3d6a3d8e7907301605f272e8c224e2c9ce7
github-action-benchmark <github@users.noreply.github.com>
2021-09-01 21:25:58 +0000

    add Benchmark (cargo) benchmark result for 6c16c99c4b77756939ed2b9cdeabba4914469b35

--- docs/bench/main/data.js
+++ docs/bench/main/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1630529760000,
+  "lastUpdate": 1630531558664,
   "repoUrl": "https://github.com/jmfiaschi/json_value_merge",
   "entries": {
     "Benchmark": [
@@ -46,6 +46,54 @@ window.BENCHMARK_DATA = {
           {
             "name": "Merge/merge_object",
             "value": 238,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jm.fiaschi@gmail.com",
+            "name": "jm.fiaschi",
+            "username": "jmfiaschi"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6c16c99c4b77756939ed2b9cdeabba4914469b35",
+          "message": "feat(project): force semantic & fix readme",
+          "timestamp": "2021-09-01T23:17:42+02:00",
+          "tree_id": "de04503856f3d06b4b5c992cd9d35e2193a3e6ce",
+          "url": "https://github.com/jmfiaschi/json_value_merge/commit/6c16c99c4b77756939ed2b9cdeabba4914469b35"
+        },
+        "date": 1630531558186,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "Merge/merge_null",
+            "value": 9,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Merge/merge_string",
+            "value": 40,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Merge/merge_array",
+            "value": 393933,
+            "range": "± 100358",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Merge/merge_object",
+            "value": 218,
             "range": "± 0",
             "unit": "ns/iter"
           }
