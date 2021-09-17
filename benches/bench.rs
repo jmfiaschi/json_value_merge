7292cb685b3e8e8484ec2bed3f4e0ef1cc470009
jm.fiaschi <jm.fiaschi@gmail.com>
2021-09-17 13:58:31 +0200

    feat(merge_in): fix bench & clean the code & add complex merge in bench (#11)
    * feat(merge_in): fix bench & clean the code & add complex merge in bench
    
    * feat(merge_in): add wildcard for array
    
    * refacto(bench): reword labels
    
    * fix(merge_in): char not covered and create an infinit loop
    
    * feat(mergin_in): raise exception if try merge object in array with wrong json pointer

--- benches/bench.rs
+++ benches/bench.rs
@@ -3,29 +3,37 @@ use json_value_merge::Merge;
 use serde_json::*;
 
 fn merge(c: &mut Criterion) {
-    let mut group = c.benchmark_group("Merge");
-    let mut json_value: Value = Value::String("my_string".to_string());
-    group.bench_function("merge_null", |b| {
-        b.iter(|| black_box(json_value.merge(Value::Null)))
+    let mut group = c.benchmark_group("merge");
+
+    let json_value: Value = Value::String("my_string".to_string());
+    group.bench_function("null", |b| {
+        b.iter(|| black_box(json_value.clone().merge(Value::Null)))
     });
-    group.bench_function("merge_string", |b| {
-        b.iter(|| black_box(json_value.merge(Value::String("my string B".to_string()))))
+    group.bench_function("string", |b| {
+        b.iter(|| {
+            black_box(
+                json_value
+                    .clone()
+                    .merge(Value::String("my string B".to_string())),
+            )
+        })
     });
-    let mut json_value: Value = serde_json::from_str(r#"["a","b"]"#).unwrap();
-    group.bench_function("merge_array", |b| {
+
+    let json_value: Value = serde_json::from_str(r#"["a","b"]"#).unwrap();
+    let json_value_to_merge: Value = serde_json::from_str(r#"["c","d"]"#).unwrap();
+    group.bench_function("array", |b| {
         b.iter(|| {
             black_box({
-                let json_value_to_merge: Value = serde_json::from_str(r#"["c","d"]"#).unwrap();
-                json_value.merge(json_value_to_merge);
+                json_value.clone().merge(json_value_to_merge.clone());
             })
         })
     });
-    let mut json_value: Value = serde_json::from_str(r#"{"a":"b"}"#).unwrap();
-    group.bench_function("merge_object", |b| {
+    let json_value: Value = serde_json::from_str(r#"{"a":"b"}"#).unwrap();
+    let json_value_to_merge: Value = serde_json::from_str(r#"{"c":"d"}"#).unwrap();
+    group.bench_function("object", |b| {
         b.iter(|| {
             black_box({
-                let json_value_to_merge: Value = serde_json::from_str(r#"{"c":"d"}"#).unwrap();
-                json_value.merge(json_value_to_merge);
+                json_value.clone().merge(json_value_to_merge.clone());
             })
         })
     });
@@ -33,38 +41,66 @@ fn merge(c: &mut Criterion) {
 }
 
 fn merge_in(c: &mut Criterion) {
-    let mut group = c.benchmark_group("Merge in");
+    let mut group = c.benchmark_group("merge_in");
 
-    let mut json_value: Value = serde_json::from_str(r#"{"field":"value"}"#).unwrap();
-    group.bench_function("merge_in_null", |b| {
-        b.iter(|| black_box(json_value.merge_in("/field", Value::Null)))
+    let json_value: Value = serde_json::from_str(r#"{"field":"value"}"#).unwrap();
+    group.bench_function("null", |b| {
+        b.iter(|| black_box(json_value.clone().merge_in("/field", Value::Null)))
     });
 
-    let mut json_value: Value = serde_json::from_str(r#"{"field":"value"}"#).unwrap();
-    group.bench_function("merge_in_string.", |b| {
-        b.iter(|| black_box(json_value.merge_in("/field", Value::String("new value".to_string()))))
+    let json_value: Value = serde_json::from_str(r#"{"field":"value"}"#).unwrap();
+    group.bench_function("string.", |b| {
+        b.iter(|| {
+            black_box(
+                json_value
+                    .clone()
+                    .merge_in("/field", Value::String("new value".to_string())),
+            )
+        })
     });
 
-    let mut json_value: Value = serde_json::from_str(r#"{"field":"value"}"#).unwrap();
-    group.bench_function("merge_in_object", |b| {
+    let json_value: Value = serde_json::from_str(r#"{"field":"value"}"#).unwrap();
+    let json_value_to_merge: Value = serde_json::from_str(r#"{"new_field":"new_value"}"#).unwrap();
+    group.bench_function("object", |b| {
         b.iter(|| {
             black_box({
-                let json_value_to_merge: Value =
-                    serde_json::from_str(r#"{"new_field":"new_value"}"#).unwrap();
-                json_value.merge_in("/field", json_value_to_merge);
+                json_value
+                    .clone()
+                    .merge_in("/field", json_value_to_merge.clone())
+                    .unwrap();
             })
         })
     });
 
-    let mut json_value: Value = serde_json::from_str(r#"["value"]"#).unwrap();
-    group.bench_function("merge_in_array", |b| {
+    let json_value: Value = serde_json::from_str(r#"["value"]"#).unwrap();
+    let json_value_to_merge: Value = serde_json::from_str(r#"["new_value"]"#).unwrap();
+    group.bench_function("array", |b| {
         b.iter(|| {
             black_box({
-                let json_value_to_merge: Value = serde_json::from_str(r#"["new_value"]"#).unwrap();
-                json_value.merge_in("/0", json_value_to_merge);
+                json_value
+                    .clone()
+                    .merge_in("/0", json_value_to_merge.clone())
+                    .unwrap();
             })
         })
     });
+
+    let json_value: Value = serde_json::from_str(r#"{"batters":{"batter":[{"id":"1001","type":"Regular"},{"id":"1002","type":"Chocolate"},{"id":"1003","type":"Blueberry"},{"id":"1004","type":"Devil's Food"}]},"id":"0001","name":"Cake","ppu":0.55,"topping":[{"id":"5001","type":"None"},{"id":"5002","type":"Glazed"},{"id":"5005","type":"Sugar"},{"id":"5007","type":"Powdered Sugar"},{"id":"5006","type":"Chocolate with Sprinkles"},{"id":"5003","type":"Chocolate"},{"id":"5004","type":"Maple"}],"type":"donut","child":{"batters":{"batter":[{"id":"1001","type":"Regular"},{"id":"1002","type":"Chocolate"},{"id":"1003","type":"Blueberry"},{"id":"1004","type":"Devil's Food"}]},"id":"0001","name":"Cake","ppu":0.55,"topping":[{"id":"5001","type":"None"},{"id":"5002","type":"Glazed"},{"id":"5005","type":"Sugar"},{"id":"5007","type":"Powdered Sugar"},{"id":"5006","type":"Chocolate with Sprinkles"},{"id":"5003","type":"Chocolate"},{"id":"5004","type":"Maple"}],"type":"donut","child":{"batters":{"batter":[{"id":"1001","type":"Regular"},{"id":"1002","type":"Chocolate"},{"id":"1003","type":"Blueberry"},{"id":"1004","type":"Devil's Food"}]},"id":"0001","name":"Cake","ppu":0.55,"topping":[{"id":"5001","type":"None"},{"id":"5002","type":"Glazed"},{"id":"5005","type":"Sugar"},{"id":"5007","type":"Powdered Sugar"},{"id":"5006","type":"Chocolate with Sprinkles"},{"id":"5003","type":"Chocolate"},{"id":"5004","type":"Maple"}],"type":"donut","child":{"batters":{"batter":[{"id":"1001","type":"Regular"},{"id":"1002","type":"Chocolate"},{"id":"1003","type":"Blueberry"},{"id":"1004","type":"Devil's Food"}]},"id":"0001","name":"Cake","ppu":0.55,"topping":[{"id":"5001","type":"None"},{"id":"5002","type":"Glazed"},{"id":"5005","type":"Sugar"},{"id":"5007","type":"Powdered Sugar"},{"id":"5006","type":"Chocolate with Sprinkles"},{"id":"5003","type":"Chocolate"},{"id":"5004","type":"Maple"}],"type":"donut","child":{"batters":{"batter":[{"id":"1001","type":"Regular"},{"id":"1002","type":"Chocolate"},{"id":"1003","type":"Blueberry"},{"id":"1004","type":"Devil's Food"}]},"id":"0001","name":"Cake","ppu":0.55,"topping":[{"id":"5001","type":"None"},{"id":"5002","type":"Glazed"},{"id":"5005","type":"Sugar"},{"id":"5007","type":"Powdered Sugar"},{"id":"5006","type":"Chocolate with Sprinkles"},{"id":"5003","type":"Chocolate"},{"id":"5004","type":"Maple"}],"type":"donut"}}}}}"#).unwrap();
+    let json_value_to_merge: Value = serde_json::from_str(r#"{"new_field":"new_value"}"#).unwrap();
+    group.bench_function("deeper_object", |b| {
+        b.iter(|| {
+            black_box({
+                json_value
+                    .clone()
+                    .merge_in(
+                        "/child/child/child/batters/batter",
+                        json_value_to_merge.clone(),
+                    )
+                    .unwrap();
+            })
+        })
+    });
+
     group.finish();
 }
 
